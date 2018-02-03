ce370458dcc29e28a369b5da21289cfab0fcec47
aploftus <aploftus@users.noreply.github.com>
2018-02-03 12:20:18 -0800

    (feat) Sketch out main app functions

--- src/app.jsx
+++ src/app.jsx
@@ -3,25 +3,47 @@ import ReactDOM from 'react-dom';
 
 import './main.css';
 
-import Header from './components/header.jsx';
-import Footer from './components/footer.jsx';
-
-import Home from './components/home.jsx';
+import Home from './components/Home.jsx';
+import SessionEnd from './components/SessionEnd.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      countdown: 0,
+      refocusCount: 0,
+      sessionLength: 60,
+
     };
   }
 
+  startCountdown() {
+    // if !countdown, countdown = session length
+    // else, countdown = countdown
+    // set interval, every second, decrement countdown
+
+    // if coundown = 0, stop the interval
+    // call endSession()
+  }
+
+  refocus() {
+    this.setState(prevState => ({ refocusCount: prevState.refocusCount + 1 }) );
+  }
+
+  endSession() {
+    // countdown = 0 if not already
+    // display user's results with the SessionEnd component
+  }
+
   render() {
+    const { countdown, refocusCount, sessionLength } = this.state;
+
     return (
       <div>
         <Header />
         <div className="container row main">
-          <Home />
+          <Home countdown={countdown} refocusCount={refocusCount} sessionLength={sessionLength} />
         </div>
         <Footer />
       </div>
